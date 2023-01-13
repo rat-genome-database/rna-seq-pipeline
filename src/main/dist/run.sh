f881c5e7454ba6e7822f19aa06c2cb0d697544d1
tutajm <mtutaj@mcw.edu>
2023-01-13 16:12:29 -0600

    updated build.gradle to be gradle 6.x + compatible

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -4,11 +4,11 @@
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 APPNAME=RNASeqPipeline
 APPDIR=/home/rgddata/pipelines/$APPNAME
-EMAILLIST=cdursun@mcw.edu
-DEVELOPER=cdursun@mcw.edu
+EMAILLIST=mtutaj@mcw.edu
+DEVELOPER=mtutaj@mcw.edu
 
-if [ "$SERVER" == "KYLE" ]; then
-  EMAILLIST=cdursun@mcw.edu,jrsmith@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST=mtutaj@mcw.edu,jrsmith@mcw.edu
 fi
 
 cd $APPDIR
@@ -17,6 +17,10 @@ DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
 LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
 export RNA_SEQ_PIPELINE_OPTS="$DB_OPTS $LOG4J_OPTS"
 bin/$APPNAME "$@" 2>&1
+
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/${APPNAME}.jar  "$@" 2>&1 > run.log
 
 mailx -s "[$SERVER] RNASeq Pipeline Summary" $EMAILLIST < $APPDIR/logs/summary.log
 
